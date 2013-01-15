e2bd9e416b4427745513d021415d4d0437476a42
FlyersWeb <contact@flyers-web.org>
2013-01-15 14:47:29 -0500

    Edited port number

--- web.js
+++ web.js
@@ -15,7 +15,7 @@ app.get('/', function(request, response) {
   response.sendfile(__dirname + '/index.html');
 });
 
-var port = process.env.PORT || 5555;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
